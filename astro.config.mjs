1d55e8da25e63012ad2af25447c56322c90dd2cb
Alessandro Mauro Incantalupo <alessandro.incantalupo@gmail.com>
2026-01-28 12:17:30 +0100

    feat: tailwind 4, daisy 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alessandroincantalupo.com',
-  integrations: [tailwind(), sitemap()]
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [sitemap()]
 });
